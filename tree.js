db7265621546ec41746e4b38dc69a3655cfe19c3
samhinton88 <sam.hinton88@gmail.com>
2022-06-30 18:09:48 +0100

    feat: binary expressions: addition

--- tree.js
+++ tree.js
@@ -3,12 +3,17 @@
  * @param {number} first 
  * @returns 
  */
-const tree = (first) => {
-    if (typeof first !== 'number') {
-        throw new Error('Value should be a number')
+const tree = (first, second, third) => {
+    if (typeof first !== undefined && second === undefined && third === undefined) {
+        if (typeof first !== 'number') {
+            throw new Error('Value should be a number')
+        }
+
+        return { result: () => first, toString: () => first.toString() }
     }
 
-    return { result: () => first, toString: () => first.toString() }
+
+    return { result: () => second + third, toString: () => `(${second} + ${third})`}
 }
 
 module.exports = tree;
--- tree.test.js
+++ tree.test.js
@@ -1,15 +1,23 @@
 const tree = require("./tree");
 
 describe("tree", () => {
-  it("should interpret usage with a single argument as just the value", () => {
-    expect(tree(2).result()).toBe(2);
+  describe("single argument usage", () => {
+    it("should interpret usage with a single argument as just the value", () => {
+      expect(tree(2).result()).toBe(2);
+    });
+
+    it("should represent usage with a single argument as the value simply", () => {
+      expect(tree(2).toString()).toBe("2");
+    });
+
+    it("should throw if given a single argument that is a numeric string", () => {
+      expect(() => tree("2").toString()).toThrow();
+    });
   });
 
-  it("should represent usage with a single argument as the value simply", () => {
-    expect(tree(2).toString()).toBe("2");
-  });
-
-  it("should throw if given a single argument that is a numeric string", () => {
-    expect(() => tree("2").toString()).toThrow();
+  describe("binary expression", () => {
+    it("should parse binary expressions with addition", () => {
+        expect(tree("+", 2, 3).result()).toBe(5);
+    })
   })
 });
