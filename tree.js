dbb20facd406335c4e60c49077b2e56f835339fd
samhinton88 <sam.hinton88@gmail.com>
2022-06-30 18:10:05 +0100

    chore: format

--- tree.js
+++ tree.js
@@ -1,19 +1,25 @@
 /**
  * tree(42) -> numeric primitive - string representation: "42"
- * @param {number} first 
- * @returns 
+ * @param {number} first
+ * @returns
  */
 const tree = (first, second, third) => {
-    if (typeof first !== undefined && second === undefined && third === undefined) {
-        if (typeof first !== 'number') {
-            throw new Error('Value should be a number')
-        }
-
-        return { result: () => first, toString: () => first.toString() }
+  if (
+    typeof first !== undefined &&
+    second === undefined &&
+    third === undefined
+  ) {
+    if (typeof first !== "number") {
+      throw new Error("Value should be a number");
     }
 
+    return { result: () => first, toString: () => first.toString() };
+  }
 
-    return { result: () => second + third, toString: () => `(${second} + ${third})`}
-}
+  return {
+    result: () => second + third,
+    toString: () => `(${second} + ${third})`,
+  };
+};
 
 module.exports = tree;
