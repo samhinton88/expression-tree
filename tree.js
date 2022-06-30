340369a617eb4c67178d3f134b7aa5d8c2350037
samhinton88 <sam.hinton88@gmail.com>
2022-06-30 18:02:55 +0100

    doc: WIP JSDOC

--- tree.js
+++ tree.js
@@ -1,3 +1,8 @@
+/**
+ * tree(42) -> numeric primitive - string representation: "42"
+ * @param {number} first 
+ * @returns 
+ */
 const tree = (first) => {
     if (typeof first !== 'number') {
         throw new Error('Value should be a number')
